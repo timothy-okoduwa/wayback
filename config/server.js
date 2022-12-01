19370a1f1fd9b7a33c31526c49690b802e124276
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2022-12-01 13:25:41 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2cca1e514aa12a1b1d34360af293dd53'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '37d922e0-5c94-4f9c-97cb-5ac24b3df511'
+};
